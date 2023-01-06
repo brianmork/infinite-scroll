897f9f60b76b80a8cb3858116b75471166b9e4b2
brian <77130771+brianmork@users.noreply.github.com>
2023-01-06 18:40:17 +0100

    Added init setup of the element creating thru API

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
+const imageContainer = document.querySelector('.image__container');
+const loader = document.querySelector('.loader');
+
+let photosArray = [];
+
 // Unsplash API
-const count = 30;
+const count = 10;
 const apiKey = '3MTe6FDy4oYTMBahvI4Y_GLbwY9g_8boyOvikcm6Mjc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Create elements for links and photos
+function displayPhotos() {
+	// Run function for each object
+	photosArray.forEach((photo) => {
+	// Create link to Unsplash
+	const item = document.createElement('a');
+	item.setAttribute('href', photo.links.html);
+	item.setAttribute('target', '_blank');
+	
+	});
+}
 
 // Get photos from Unsplash API
 async function getPhotos() {
 	try {
 		const response = await fetch(apiUrl);
-		const data = await response.json();
-		console.log(data);
+		photosArray = await response.json();
+		console.log(photosArray);
+		displayPhotos();
 	} catch (error) {
 		// Catch error here
 	}
