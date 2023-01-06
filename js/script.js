8f7cacf75729da06639db488081d7991f4b64e58
brian <77130771+brianmork@users.noreply.github.com>
2023-01-06 18:49:23 +0100

    Completed creation of elements through the API

--- js/script.js
+++ js/script.js
@@ -4,19 +4,26 @@ const loader = document.querySelector('.loader');
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '3MTe6FDy4oYTMBahvI4Y_GLbwY9g_8boyOvikcm6Mjc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Create elements for links and photos
-function displayPhotos() {
-	// Run function for each object
-	photosArray.forEach((photo) => {
-	// Create link to Unsplash
-	const item = document.createElement('a');
-	item.setAttribute('href', photo.links.html);
-	item.setAttribute('target', '_blank');
-	
+	function displayPhotos() {
+		// Run function for each object
+		photosArray.forEach((photo) => {
+		// Create link to Unsplash
+		const item = document.createElement('a');
+		item.setAttribute('href', photo.links.html);
+		item.setAttribute('target', '_blank');
+		// Create image
+		const img = document.createElement('img');
+		img.setAttribute('src', photo.urls.regular);
+		img.setAttribute('alt', photo.alt_description);
+		img.setAttribute('title', photo.alt_description);
+		// Add image to a into container
+		item.appendChild(img);
+		imageContainer.appendChild(item);
 	});
 }
 
