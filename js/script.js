f18ae93d0aea28a1797bf865582d1bf5aa959bb1
brian <77130771+brianmork@users.noreply.github.com>
2023-01-06 19:02:26 +0100

    Made the code DRY

--- js/script.js
+++ js/script.js
@@ -4,9 +4,16 @@ const loader = document.querySelector('.loader');
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+const count = 10;
 const apiKey = '3MTe6FDy4oYTMBahvI4Y_GLbwY9g_8boyOvikcm6Mjc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Attributes function
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
 
 // Create elements for links and photos
 	function displayPhotos() {
@@ -14,13 +21,17 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 		photosArray.forEach((photo) => {
 		// Create link to Unsplash
 		const item = document.createElement('a');
-		item.setAttribute('href', photo.links.html);
-		item.setAttribute('target', '_blank');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank',
+		})
 		// Create image
 		const img = document.createElement('img');
-		img.setAttribute('src', photo.urls.regular);
-		img.setAttribute('alt', photo.alt_description);
-		img.setAttribute('title', photo.alt_description);
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description,
+		})
 		// Add image to a into container
 		item.appendChild(img);
 		imageContainer.appendChild(item);
